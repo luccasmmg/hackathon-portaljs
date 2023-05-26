79f1a8d87dea21e5013cf8d82148208efe93b1cf
Luccas Mateus de Medeiros Gomes <luccasmmg@gmail.com>
2023-05-26 13:23:40 -0300

    initial commit

--- pages/@org/[org]/[...path].tsx
+++ pages/@org/[org]/[...path].tsx
@@ -6,8 +6,9 @@ import { getProject, GithubProject } from "../../../lib/octokit";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import Breadcrumbs from "../../../components/_shared/Breadcrumbs";
-import parse from '../../../lib/markdown';
-import DataRichDocument from '../../../components/DataRichDocument'
+import parse from "../../../lib/markdown";
+import DataRichDocument from "../../../components/DataRichDocument";
+import { FlatUiTable } from "@portaljs/components";
 
 export default function ProjectPage({ project }) {
   const repoId = `@${project.repo_config.owner}/${project.repo_config.repo}`;
@@ -19,55 +20,63 @@ export default function ProjectPage({ project }) {
           project.base_path !== "/" ? "/" + project.base_path : ""
         } - GitHub Datasets`}
       />
-      <main className="prose mx-auto my-8">
-        <Breadcrumbs links={[{ title: repoId, href: "" }]} />
-        <h1 className="mb-0 mt-16">{project.repo_config.name || repoId}</h1>
-        <p className="mb-8">
-          <span className="font-semibold">Repository:</span>{" "}
-          <a target="_blank" href={project.html_url}>
-            {project.html_url}
-          </a>
-        </p>
+      <div className="bg-gray-200 py-8">
+        <main className="prose mx-auto my-8">
+          <Breadcrumbs links={[{ title: repoId, href: "" }]} />
+          <h1 className="mb-0 mt-16">{project.repo_config.name || repoId}</h1>
+          <p className="mb-8">
+            <span className="font-semibold">Repository:</span>{" "}
+            <a target="_blank" href={project.html_url}>
+              {project.html_url}
+            </a>
+          </p>
 
-        <h2 className="mb-0 mt-10">Files</h2>
-        <div className="inline-block min-w-full py-2 align-middle">
-          <table className="min-w-full divide-y divide-gray-300">
-            <thead>
-              <tr>
-                <th
-                  scope="col"
-                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                >
-                  Name
-                </th>
-                <th
-                  scope="col"
-                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                >
-                  Size
-                </th>
-              </tr>
-            </thead>
-            <tbody className="divide-y divide-gray-200">
-              {project.files.map((file) => (
-                <tr key={file.download_url}>
-                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                    <a href={file.download_url}>{file.name}</a>
-                  </td>
-                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                    {file.size} Bytes
-                  </td>
+          <h2 className="mb-0 mt-10">Files</h2>
+          <div className="inline-block min-w-full py-2 align-middle">
+            <table className="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Size
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+              </thead>
+              <tbody className="divide-y divide-gray-200">
+                {project.files.map((file) => (
+                  <tr key={file.download_url}>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      <a href={file.download_url}>{file.name}</a>
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      {file.size} Bytes
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
 
-        <hr />
+          <h2 className="uppercase font-black">Data Previews</h2>
+          <div>
+            {project.files.map((file) => (
+              <FlatUiTable url={file.download_url} />
+            ))}
+          </div>
+          <hr />
 
-        <h2 className="uppercase font-black">Readme</h2>
-        <DataRichDocument source={project.mdxSource} />
-      </main>
+          <h2 className="uppercase font-black">Readme</h2>
+          <DataRichDocument source={project.mdxSource} />
+        </main>
+      </div>
     </>
   );
 }
@@ -119,7 +128,9 @@ export async function getStaticProps({ params }) {
   });
   const github_pat = getConfig().serverRuntimeConfig.github_pat;
   const project = await getProject(repo, github_pat);
-  let { mdxSource, frontMatter } = await parse(project.readmeContent, '.mdx', { project });
+  let { mdxSource, frontMatter } = await parse(project.readmeContent, ".mdx", {
+    project,
+  });
   return {
     props: {
       project: { ...project, repo_config: repo, mdxSource },
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import ExternalLinkIcon from "../components/icons/ExternalLinkIcon";
 import TimeAgo from "react-timeago";
 import Link from "next/link";
 import { NextSeo } from "next-seo";
+import { MagnifyingGlassIcon, LinkIcon } from "@heroicons/react/20/solid";
 
 export async function getStaticProps() {
   const jsonDirectory = path.join(process.cwd(), "/datasets.json");
@@ -29,109 +30,83 @@ export function Datasets({ projects }) {
   return (
     <>
       <NextSeo title="GitHub Datasets" />
-      <div className="bg-white min-h-screen">
+      <div className="bg-gray-200 min-h-screen">
         <div className="mx-auto max-w-7xl px-6 py-16 sm:py-24 lg:px-8">
-          <div className="text-center">
-            <h2 className="text-3xl font-bold leading-10 tracking-tight">
-              GitHub Datasets
-            </h2>
-            <p className="mt-3 mx-auto max-w-2xl text-base leading-7 text-gray-500">
-              Data catalog with datasets hosted on GitHub by{" "}
-              <Link
-                target="_blank"
-                className="underline"
-                href="https://portaljs.org/"
-              >
-                🌀 PortalJS
-              </Link>
-            </p>
+          <div className="py-8">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+              <div className="mx-auto max-w-2xl lg:mx-0">
+                <p className="text-base font-semibold leading-7 text-indigo-600">
+                  Get the data you need
+                </p>
+                <h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                  Climate change data
+                </h2>
+                <p className="mt-6 text-lg leading-8 text-gray-600">
+                  Data about climate change mainly CO2 Emissions
+                </p>
+              </div>
+            </div>
           </div>
-          <div className="mt-20">
-            <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-              <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-                <table className="min-w-full divide-y divide-gray-300">
-                  <thead>
-                    <tr>
-                      <th
-                        scope="col"
-                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                      >
-                        Name
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                      >
-                        Repository
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                      >
-                        Description
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                      >
-                        Last updated
-                      </th>
-                      <th
-                        scope="col"
-                        className="relative py-3.5 pl-3 pr-4 sm:pr-0"
-                      ></th>
-                    </tr>
-                  </thead>
-                  <tbody className="divide-y divide-gray-200">
-                    {projects.map((project) => (
-                      <tr key={project.id}>
-                        <td className="whitespace-nowrap px-3 py-6 text-sm text-gray-500">
-                          {project.repo_config.name
-                            ? project.repo_config.name
-                            : project.full_name +
-                              (project.base_path === "/"
-                                ? ""
-                                : "/" + project.base_path)}
-                        </td>
-                        <td className="whitespace-nowrap px-3 py-6 text-sm group text-gray-500 hover:text-gray-900 transition-all duration-250">
-                          <a
-                            href={project.html_url}
-                            target="_blank"
-                            className="flex items-center"
-                          >
-                            @{project.full_name}{" "}
-                            <ExternalLinkIcon className="ml-1" />
-                          </a>
-                        </td>
-                        <td className="px-3 py-4 text-sm text-gray-500">
-                          {project.repo_config.description
-                            ? project.repo_config.description
-                            : project.description}
-                        </td>
-                        <td className="whitespace-nowrap px-3 py-6 text-sm text-gray-500">
-                          <TimeAgo date={new Date(project.last_updated)} />
-                        </td>
-                        <td className="relative whitespace-nowrap py-6 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+            <div className="mx-4 my-2 overflow-x-auto sm:mx-6 lg:mx-8">
+              <ul
+                role="list"
+                className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
+              >
+                {projects.map((project) => (
+                  <li
+                    key={project.readme}
+                    className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white shadow"
+                  >
+                    <div className="flex w-full flex-1 items-start justify-between space-x-6 p-6">
+                      <div className="flex-1">
+                        <div className="flex items-start space-x-3">
+                          <h3 className="truncate text-sm font-medium text-gray-900">
+                            {project.name}
+                          </h3>
+                        </div>
+                        <p className="mt-1 text-sm text-gray-500">
+                          {project.repo_config.description}
+                        </p>
+                      </div>
+                    </div>
+                    <div>
+                      <div className="-mt-px flex divide-x divide-gray-200">
+                        <div className="flex w-0 flex-1">
                           <a
                             href={`/@${project.repo_config.owner}/${
                               project.repo_config.repo
                             }/${
                               project.base_path === "/" ? "" : project.base_path
                             }`}
-                            className="border border-gray-900 text-gray-900 px-4 py-2 transition-all hover:bg-gray-900 hover:text-white"
+                            className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
                           >
-                            info
+                            <MagnifyingGlassIcon
+                              className="h-5 w-5 text-gray-400"
+                              aria-hidden="true"
+                            />
+                            Preview
                           </a>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
+                        </div>
+                        <div className="-ml-px flex w-0 flex-1">
+                          <a
+                            href={project.html_url}
+                            className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
+                          >
+                            <LinkIcon
+                              className="h-5 w-5 text-gray-400"
+                              aria-hidden="true"
+                            />
+                            Repo
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
         </div>
-      </div>
     </>
   );
 }
